925de1c9e1ce51ca63dd62184c05fa34b5ff9183
alex <alex.hmn18@gmail.com>
2024-11-08 18:51:22 +0100

    Réinitialisation mot de passe avec reset-password-bundle

---
+++ src/Form/ResetPasswordRequestFormType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ResetPasswordRequestFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', TextType::class, [
+                'attr' => ['autocomplete' => 'email'],
+                'required' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Entrez un email',
+                    ]),
+                    new Email([
+                        'message' => 'Entre un email valide'
+                    ])
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
